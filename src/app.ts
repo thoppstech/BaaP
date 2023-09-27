548169ab2553570901fe5d200dcfcc28dc5f61b8
Abu Thoppan <abuthoppan@thopps.com>
2023-09-27 20:31:35 +0800

    added echo test apis

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import config from 'config';
 import logger from './utils/logger'
 import routes from './routes';
 
+
 const port:number = config.get<number>('port');
 const app = express();
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.listen(port,()=>{
     logger.info(`App is running at port ${port}`);
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,37 @@
-import { Express,Request,Response } from "express";
+import { Express, Request, Response } from "express";
+import logger from './utils/logger'
 
-function routes(app:Express){
-    app.get("/healthcheck",(req:Request,res:Response)=>res.sendStatus(200))
+
+function routes(app: Express) {
+    app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+    app.get("/echo", (req: Request, res: Response) => {
+        const method = req.method;
+        const queryParams = req.query;
+        const body = req.body;
+        logger.info({ queryParams, body });
+        res.send({ method, queryParams, body })
+    });
+    app.post("/echo", (req: Request, res: Response) => {
+        const method = req.method;
+        const queryParams = req.query;
+        const body = req.body;
+        logger.info({ queryParams, body });
+        res.send({ method, queryParams, body })
+    });
+    app.put("/echo", (req: Request, res: Response) => {
+        const method = req.method;
+        const queryParams = req.query;
+        const body = req.body;
+        logger.info({ queryParams, body });
+        res.send({ method, queryParams, body })
+    });
+    app.delete("/echo", (req: Request, res: Response) => {
+        const method = req.method;
+        const queryParams = req.query;
+        const body = req.body;
+        logger.info({ queryParams, body });
+        res.send({ method, queryParams, body })
+    });
 }
 
 export default routes;
